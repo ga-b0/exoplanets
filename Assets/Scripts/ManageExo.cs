999ef0a429d97f56c7661491caa0dc26b7868e4a
Gabriel Calderon <gabrielvilleda8@gmail.com>
2024-06-18 12:13:28 -0600

    Modificacion archivo que lee json para que funciones con WEBGL

--- Assets/Scripts/ManageExo.cs
+++ Assets/Scripts/ManageExo.cs
@@ -3,31 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
 using System.IO;
+using UnityEngine.Networking;
 
 public class ManageExo : MonoBehaviour
 {
-    private string filteredFilePath = "fetchFiltered.json";
+    private string jsonFileName = "fetchFiltered.json";
     public List<Planet> planets;
+
     private void Awake()
     {
-        string jsonData = File.ReadAllText(Path.Combine(Application.streamingAssetsPath, filteredFilePath));
-        planets = JsonConvert.DeserializeObject<List<Planet>>(jsonData);
-        Debug.Log("Count: " + planets.Count);
+        StartCoroutine(LoadJson());
     }
 
-    void Update()
+    private IEnumerator LoadJson()
     {
-        /*foreach (var planet in planets)
+        string filePath = Path.Combine(Application.streamingAssetsPath, jsonFileName);
+
+#if UNITY_WEBGL
+        UnityWebRequest www = UnityWebRequest.Get(filePath);
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
         {
-            Debug.Log($"Kepler Name: {planet.kepler_name}, Koi Prad: {planet.koi_prad}");
-        }*/
+            Debug.LogError("Error JSON: " + www.error);
+        }
+        else
+        {
+            string jsonData = www.downloadHandler.text;
+            planets = JsonConvert.DeserializeObject<List<Planet>>(jsonData);
+            Debug.Log("Count: " + planets.Count);
+        }
+#else
+
+        if (File.Exists(filePath))
+        {
+            string jsonData = File.ReadAllText(filePath);
+            planets = JsonConvert.DeserializeObject<List<Planet>>(jsonData);
+            Debug.Log("Count: " + planets.Count);
+        }
+        else
+        {
+            Debug.LogError("Error JSON: " + filePath);
+        }
+#endif
     }
 
-
-    public class Planet {
-
+    public class Planet
+    {
         public string kepler_name { get; set; }
         public float? koi_prad { get; set; }
-
     }
 }
